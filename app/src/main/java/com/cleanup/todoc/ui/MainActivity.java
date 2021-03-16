eb2f8c82951013592379cad0b525d1813d6d67c9
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-16 17:02:06 +0100

    improvement: migrate to androidx

--- app/src/main/java/com/cleanup/todoc/ui/MainActivity.java
+++ app/src/main/java/com/cleanup/todoc/ui/MainActivity.java
@@ -2,12 +2,12 @@ package com.cleanup.todoc.ui;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
