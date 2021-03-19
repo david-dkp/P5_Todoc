8e27662a28bf6457cf2788c63d40cafb4b7c069b
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-19 13:41:27 +0100

    feat: create ProjectEntity

---
+++ app/src/main/java/com/cleanup/todoc/data/local/todocdatabase/entities/ProjectEntity.java
@@ -0,0 +1,45 @@
+package com.cleanup.todoc.data.local.todocdatabase.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "project_table")
+public class ProjectEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private String name;
+
+    private int color;
+
+    public ProjectEntity(long id, String name, int color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
