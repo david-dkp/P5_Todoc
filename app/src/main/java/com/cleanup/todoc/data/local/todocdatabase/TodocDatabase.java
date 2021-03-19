0f2b29a2ae2f24c3743d014eead95e86a548467c
David DEKEUWER <david.dekeuwer@gmail.com>
2021-03-19 13:51:42 +0100

    feat: add entities declaration to database, add singleton pattern

--- app/src/main/java/com/cleanup/todoc/data/local/todocdatabase/TodocDatabase.java
+++ app/src/main/java/com/cleanup/todoc/data/local/todocdatabase/TodocDatabase.java
@@ -1,11 +1,32 @@
 package com.cleanup.todoc.data.local.todocdatabase;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.cleanup.todoc.data.local.todocdatabase.entities.ProjectEntity;
+import com.cleanup.todoc.data.local.todocdatabase.entities.TaskEntity;
+
+import static com.cleanup.todoc.others.Constants.TODOC_DATABASE_NAME;
 import static com.cleanup.todoc.others.Constants.TODOC_DATABASE_VERSION;
 
-@Database(entities = {}, version = TODOC_DATABASE_VERSION)
+@Database(entities = {TaskEntity.class, ProjectEntity.class}, version = TODOC_DATABASE_VERSION)
 public abstract class TodocDatabase extends RoomDatabase {
+    private static TodocDatabase instance;
 
+    public abstract TodocDao getTodocDao();
+
+    public static TodocDatabase getInstance(Context context) {
+        if (instance == null) {
+            return Room.databaseBuilder(
+                    context,
+                    TodocDatabase.class,
+                    TODOC_DATABASE_NAME
+            ).build();
+        }
+
+        return instance;
+    }
 }
--- app/src/main/java/com/cleanup/todoc/others/Constants.java
+++ app/src/main/java/com/cleanup/todoc/others/Constants.java
@@ -1,5 +1,7 @@
 package com.cleanup.todoc.others;
 
 public class Constants {
+    //DB
     public static final int TODOC_DATABASE_VERSION = 1;
+    public static final String TODOC_DATABASE_NAME = "todoc.db";
 }
